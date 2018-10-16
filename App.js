223ed585626e6706e91ede85dba90e8b72869b81
rlterry00 <chuckterry00@gmail.com>
2018-10-16 00:47:43 -0400

    fixed path

---
+++ App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+      <View style={styles.header}>
+      </View>
+        <View style={styles.contactOne}>
+        <Text style={styles.white}>Contact</Text>
+        </View>
+        <View style={styles.contactTwo}>
+        <Text style={styles.white}>Contact</Text>
+        </View>
+        <View style={styles.contactThree}>
+        <Text style={styles.white}>Contact</Text>
+        </View>
+        <View style={styles.footer}>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  header: {
+    flex: 1,
+  },
+  footer: {
+    flex: 1,
+  },
+  white: {
+    color: 'white',
+  },
+  contactOne: {
+    flex: 3,
+    alignItems: 'center',
+    backgroundColor: 'orange', 
+  },
+  contactTwo: {
+    flex: 3,
+    alignItems: 'center',
+    backgroundColor: 'green', 
+  },
+  contactThree: {
+    flex: 3,
+    alignItems: 'center',
+    backgroundColor: 'blue', 
+  },
+});
